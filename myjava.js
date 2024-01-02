6552f626aff4bd878df76ec232395b08b018399b
ashutosh <ashutoshmathur1711@gmail.com>
2024-01-02 18:58:37 +0530

    made score intact after reloading of page also

--- myjava.js
+++ myjava.js
@@ -1,9 +1,19 @@
+let ext=localStorage.getItem('decision');
+
+let decision=JSON.parse(ext) || {
+    win:0,
+    lost:0,
+    tie:0,
+};
+
+function myfunc2(){
+    localStorage.clear();
+    location.reload();
+}
 function myfunc(mychoice){
-    
-    
+   
     let num= Math.random()*3;
         let compchoice;
-        
         if(num>=0 && num<=1){
              compchoice='bat';
         }
@@ -16,36 +26,38 @@ function myfunc(mychoice){
         let result;
         if(mychoice===compchoice){
             result=`Its a Tie`;
-            
+            decision.tie++;
         }
         else{
             if(mychoice==='bat'){
                 if(compchoice==='ball'){
                     result=`You Won`;
+                    decision.win++;
                     
                 }
                 else if(compchoice==='stump'){
                     result=`Computer Won`;
-                    
+                    decision.lost++;
                 }    
             }
             if(mychoice==='ball'){
                 if(compchoice==='bat'){
                     result=`Computer Won`;
-                    
+                    decision.lost++;
                 }
                 else if(compchoice==='stump'){
                     result=`You Won`;
-                    
+                    decision.win++;
                 }    
             }
             if(mychoice==='stump'){
                 if(compchoice==='ball'){
                     result=`Computer Won`;
-                    
+                    decision.lost++;
                 }
                 else if(compchoice==='bat'){
                     result=`You Won`;
+                    decision.win++;
                     
                 }    
             }
@@ -62,9 +74,14 @@ function myfunc(mychoice){
         }
        document.querySelector("#p1").innerHTML=mychoice;
        document.querySelector("#p2").innerHTML=compchoice;
+       let win=decision.win;
+       let lost=decision.lost;
+       let tie=decision.tie;
+
+       document.querySelector('#p3').innerHTML=`Win: ${win} Lost: ${lost} Tie: ${tie}`;
         document.querySelector('#output').innerHTML= result;
-       
-        
+        localStorage.setItem('decision', JSON.stringify(decision));
+        console.log(decision);
         
 }
 
